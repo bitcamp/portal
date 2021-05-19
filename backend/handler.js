a3067d48c129f23e467c9087d661ae6cd5fc3566
Dhanvee Ivaturi <dhanvee@umd.edu>
2021-05-19 01:56:25 -0700

    add bare-minimum backend service

--- backend/handler.js
+++ backend/handler.js
@@ -1,18 +1,43 @@
-'use strict';
+const AWS = require('aws-sdk');
+const withSentry = require('serverless-sentry-lib');
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+AWS.config.update({ region: 'us-east-1' });
+
+// POST /register - Adds a new registration to the database
+module.exports.register = withSentry(async (event) => {
+  const body = JSON.parse(event.body);
+  const ddb = new AWS.DynamoDB.DocumentClient();
+
+  // Checks if any field is missing
+  if (!body.email
+   || !body.name
+   || !body.school) {
+    return {
+      statusCode: 500,
+      body: '/register is missing a field',
+    };
+  }
+
+  const params = {
+    TableName: process.env.REGISTRATION_TABLE,
+    Item: {},
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  // Dynamically add post request body params to document
+  Object.keys(body).forEach((k) => {
+    params.Item[k] = body[k];
+  });
+
+  // Call DynamoDB to add the item to the table
+  await ddb.put(params).promise();
+
+  // Returns status code 200 and JSON string of 'result'
+  return {
+    statusCode: 200,
+    body: JSON.stringify(params.Item),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  };
+});
