a9431836756e05b1f71cbaf45d495ee8f3810a74
Dhanvee Ivaturi <dhanvee@umd.edu>
2021-05-19 05:02:05 -0700

    deploy stage and prod, add configs

--- frontend/src/config/general.js
+++ frontend/src/config/general.js
@@ -3,11 +3,10 @@ export default {
   dev: {
     BACKEND_ENDPOINT: 'https://wxi0zamgi3.execute-api.us-east-1.amazonaws.com',
   },
-  // TODO: still need to deploy/setup stage and prod
-  // stage: {
-  //     BACKEND_ENDPOINT: 'https://wxi0zamgi3.execute-api.us-east-1.amazonaws.com',
-  // },
-  // prod: {
-  //     BACKEND_ENDPOINT: 'https://wxi0zamgi3.execute-api.us-east-1.amazonaws.com',
-  // },
+  stage: {
+      BACKEND_ENDPOINT: 'https://5sz8efad61.execute-api.us-east-1.amazonaws.com',
+  },
+  prod: {
+      BACKEND_ENDPOINT: 'https://c22iypqn2h.execute-api.us-east-1.amazonaws.com',
+  },
 };
---
+++ frontend/vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    disableHostCheck: true,
+  },
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'us-east-1',
+      bucket: 'register.gotechnica.org',
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5,
+    },
+  },
+};
