6a0bd6cdaa865dda9fe1da1297e2e792947a4722
farisalipk <81818189+farisalipk@users.noreply.github.com>
2021-05-22 18:18:01 -0400

    initial thank you page complete (#17)
    * initial thank you page complete:
    
    :
    
    * Updated Cyan Dots on Thank Yyou Page
    
    * restored old dots
    
    * Finalized background position and dot colors
    
    * attempted fix on package-lock.json
    
    * fixing conflict
    
    * edited margins, added rounded corners
    
    * temporary commit
    
    * merged common css styles for dots into global
    
    * moved .dot style into scoped style in Thanks.vue

--- frontend/src/views/Thanks.vue
+++ frontend/src/views/Thanks.vue
@@ -1,9 +1,46 @@
 <template>
   <div class="about center">
-    <b-container class="mt-4">
-      <b-card>
-        <h1>Thanks for registering!</h1>
+    <b-container class="mt-5">
+      <b-card style="border-radius: 1rem">
+        <h1>Thanks, First Name!</h1>
+        <p class= "purple-text" > <!-- making paragraphs have larger margins than header -->
+          You will be sent a confirmation email for your registration. Keep in touch with Technica by following us on social media or joining our Community listerv!
+        </p>
+        <p class= "purple-text"> <!-- making paragraphs have larger margins than header -->
+          Welcome to the TechniFAM. Get ready for the best weekend you've ever had.
+        </p>
+        <span class="dot dot1"></span>
+        <span class="dot dot2"></span>
+        <span class="dot dot3"></span>
+        <br>
+        <span class="dot dot1"></span>
+        <span class="dot dot2"></span>
+        <span class="dot dot3"></span>
+
       </b-card>
     </b-container>
   </div>
 </template>
+
+<style scoped>
+
+  .dot {
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    margin-right: 10px;
+    margin-left: 10px;
+    margin-bottom: 5px;
+    margin-top: 5px;
+    display: inline-block;
+  }
+  .dot1 {
+    background-color: var(--muted-cyan);
+  }
+  .dot2 {
+    background-color: var(--lavender);
+  }
+  .dot3 {
+    background-color: var(--rose);
+  }
+</style>
