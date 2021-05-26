73b5f8bcdb5917832b200bcd6093f75a67c3337a
Dhanvee Ivaturi <dhanvee@umd.edu>
2021-05-26 00:35:31 -0700

    Add referral generation (#34)

--- backend/handler.js
+++ backend/handler.js
@@ -23,10 +23,28 @@ module.exports.register = withSentry(async (event) => {
     Item: {},
   };
 
+  // TODO: maybe do it manually instead of dynamically
   // Dynamically add post request body params to document
   Object.keys(body).forEach((k) => {
     params.Item[k] = body[k];
   });
+
+  // Generate referral ID
+  const referralBase = body.name.split(" ")[0].toLowerCase();
+  var referralID;
+  do {
+    referralID = referralBase + "-" + makeAddon(3);
+    var referralQuery = {
+      TableName: process.env.REGISTRATION_TABLE,
+      IndexName: "referralsIndex",
+      KeyConditionExpression: "referral_id = :v_refer",
+      ExpressionAttributeValues: { ":v_refer": referralID }
+  };
+    var resp = await ddb.query(referralQuery).promise();
+  } while (resp.Count != 0);
+
+  // Add referral id to user
+  params.Item["referral_id"] = referralID;
 
   // Call DynamoDB to add the item to the table
   await ddb.put(params).promise();
@@ -41,3 +59,12 @@ module.exports.register = withSentry(async (event) => {
     },
   };
 });
+
+const makeAddon = (length) => {
+  var result = [];
+  var chars = 'abcdefghjkmnpqrstuvwxyz23456789'; // avoid i, l , o, 0, 1
+  for ( var i = 0; i < length; i++ ) {
+    result.push(chars.charAt(Math.floor(Math.random() * chars.length)));
+  }
+  return result.join('');
+}
--- frontend/src/config/general.js
+++ frontend/src/config/general.js
@@ -1,7 +1,7 @@
 export default {
   shared: {},
   dev: {
-    BACKEND_ENDPOINT: 'https://kciwkosmf4.execute-api.us-east-1.amazonaws.com',
+    BACKEND_ENDPOINT: 'https://7a55navjb9.execute-api.us-east-1.amazonaws.com',
   },
   stg: {
       BACKEND_ENDPOINT: 'https://zyt42ijl8e.execute-api.us-east-1.amazonaws.com',
