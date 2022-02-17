1186e3f8d6c15238ecd98799b08ba647822b8b03
Shu-Huai Sean Lin <slin185@terpmail.umd.edu>
2022-02-17 16:48:35 -0500

    added confirmation email

--- frontend/src/components/SocialLinks.vue
+++ frontend/src/components/SocialLinks.vue
@@ -1,35 +1,40 @@
 <template>
   <div class="social-buttons">
-      <a href="https://www.youtube.com/channel/UC-Wo4UqhGUKQEp4o-51VMIw/" target="_blank">
+      <a href="https://www.youtube.com/channel/UC_F_7Hv3SAUcQBubYWD-2BA" target="_blank">
         <div class="blue-circle">
             <img src="../assets/w_youtube.svg" alt="YouTube icon">
           </div>
       </a>
-      <a href="https://twitter.com/gotechnica?lang=en" target="_blank">
+      <a href="https://twitter.com/bitcmp?lang=en" target="_blank">
         <div class="blue-circle">
             <img src="../assets/w_twitter.svg" alt="Twitter icon">
           </div>
       </a>
-      <a href="https://www.facebook.com/gotechnica/" target="_blank">
+      <a href="https://www.facebook.com/bitcmp/" target="_blank">
         <div class="purple-circle">
             <img src="../assets/w_facebook.svg" alt="Facebook icon">
         </div>
       </a>
-      <a href="https://www.instagram.com/gotechnica/" target="_blank">
+      <a href="https://www.instagram.com/bitcamp/" target="_blank">
         <div class="purple-circle">
             <img src="../assets/w_instagram.svg" alt="Instagram icon">
         </div>
       </a>
-      <a href="https://medium.com/gotechnica" target="_blank" >
+      <a href="https://bitcmp.medium.com/" target="_blank" >
         <div class="pink-circle">
             <img src="../assets/w_medium.svg" alt="Medium icon">
         </div>
       </a>
-      <a href="https://linkedin.com/company/gotechnica" target="_blank">
+      <a href="https://www.linkedin.com/company/bitcamp/" target="_blank">
         <div class="pink-circle">
             <img src="../assets/w_linkedin.svg" alt="LinkedIn icon">
         </div>
       </a>
+      <!-- <a href="https://www.tiktok.com/@bitcamp_umd" target="_blank">
+        <div class="pink-circle">
+            <img src="../assets/w_linkedin.svg" alt="TikTok icon">
+        </div>
+      </a> -->
     </div>
 </template>
 
