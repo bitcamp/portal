eb3ac0d1fc824036965024a58ed0eed10caf8f93
Tyson Cheung <cheungtyson@gmail.com>
2024-02-03 21:14:59 -0500

    Title buttons + styling

--- frontend/src/components/CardButton.vue
+++ frontend/src/components/CardButton.vue
@@ -1,13 +1,13 @@
 <template>
-  <b-card class="role-card" @click="redirectToRole">
-    <!-- Placeholder for Logo (e.g filled in circle?) -->
-    <!-- <b-avatar
-    </b-avatar> -->
+  <b-card class="role-card">
+    <img v-if="svgImagePath" :src="require(`@/assets/${svgImagePath}`)" class="role-logo" />
 
     <!-- Role Title -->
-    <h2 class="role-title">
-      {{ roleTitle }}
-    </h2>
+    <div class="title-container" :style="{ backgroundColor: titleBackgroundColor }" @click="redirectToRole">      
+      <h2 class="role-title">
+        {{ roleTitle }}
+      </h2>
+    </div>
 
     <!-- Role Description -->
     <b-card-text class="role-description">
@@ -18,11 +18,12 @@
 
 <script>
 export default {
-  name: "CardButton",
+  name: "CardButtonComponent",
   props: {
     roleTitle: String,
-    roleDescription: String,
-    roleURL: String, // The route path to redirect when the card is clicked
+    roleDescription: String, 
+    svgImagePath: String, // marshie logo
+    roleURL: String, // route path to redirect when the card is clicked
   },
   methods: {
     redirectToRole() {
@@ -34,9 +35,60 @@ export default {
 </script>
 
 <style scoped>
+.role-card {
+  background-color: #ffffff00;
+  border: none;
+}
 .role-title {
   font-size: calc(0.8rem + 1vw);
+  font-family: "M PLUS Rounded 1c", sans-serif;
+  border-radius: 5px;
+  padding: 5px 20px;
+  margin-bottom: 0;
 }
+
+.title-container {
+  display: inline-block; 
+  cursor: pointer;
+  padding: 3px 4px;
+  border-radius: 10px;
+  margin: 20px;
+}
+
+/* Specific styles for each button */
+.role-card[data-role="hacker"] .role-title {
+  background-color: #B42F2F;
+  color: #FFB169;
+  border: 3px solid #E03728;
+  text-shadow: -2px -2px 0 #E03728,-2px -1px 0 #E03728,-2px 0px 0 #E03728,-2px 1px 0 #E03728,-2px 2px 0 #E03728,-1px -2px 0 #E03728,-1px -1px 0 #E03728,-1px 0px 0 #E03728,-1px 1px 0 #E03728,-1px 2px 0 #E03728,0px -2px 0 #E03728,0px -1px 0 #E03728,0px 0px 0 #E03728,0px 1px 0 #E03728,0px 2px 0 #E03728,1px -2px 0 #E03728,1px -1px 0 #E03728,1px 0px 0 #E03728,1px 1px 0 #E03728,1px 2px 0 #E03728,2px -2px 0 #E03728,2px -1px 0 #E03728,2px 0px 0 #E03728,2px 1px 0 #E03728,2px 2px 0 #E03728
+}
+.role-card[data-role="hacker"] .title-container {
+  background-color: #FFB169;
+  border: 3px solid #E03728;
+}
+
+.role-card[data-role="mentor"] .role-title {
+  background-color: #52A4D2;
+  color: #DBF2FF;
+  border: 3px solid #51BEFA;
+  text-shadow: -2px -2px 0 #51BEFA,-2px -1px 0 #51BEFA,-2px 0px 0 #51BEFA,-2px 1px 0 #51BEFA,-2px 2px 0 #51BEFA,-1px -2px 0 #51BEFA,-1px -1px 0 #51BEFA,-1px 0px 0 #51BEFA,-1px 1px 0 #51BEFA,-1px 2px 0 #51BEFA,0px -2px 0 #51BEFA,0px -1px 0 #51BEFA,0px 0px 0 #51BEFA,0px 1px 0 #51BEFA,0px 2px 0 #51BEFA,1px -2px 0 #51BEFA,1px -1px 0 #51BEFA,1px 0px 0 #51BEFA,1px 1px 0 #51BEFA,1px 2px 0 #51BEFA,2px -2px 0 #51BEFA,2px -1px 0 #51BEFA,2px 0px 0 #51BEFA,2px 1px 0 #51BEFA,2px 2px 0 #51BEFA}
+
+.role-card[data-role="mentor"] .title-container {
+  background-color: #DBF2FF;
+  border: 3px solid #51BEFA;
+}
+
+.role-card[data-role="volunteer"] .role-title {
+  background-color: #368D5A;
+  color: #E9FFE6;
+  border: 3px solid #3A9D63;
+  text-shadow: -2px -2px 0 #3A9D63,-2px -1px 0 #3A9D63,-2px 0px 0 #3A9D63,-2px 1px 0 #3A9D63,-2px 2px 0 #3A9D63,-1px -2px 0 #3A9D63,-1px -1px 0 #3A9D63,-1px 0px 0 #3A9D63,-1px 1px 0 #3A9D63,-1px 2px 0 #3A9D63,0px -2px 0 #3A9D63,0px -1px 0 #3A9D63,0px 0px 0 #3A9D63,0px 1px 0 #3A9D63,0px 2px 0 #3A9D63,1px -2px 0 #3A9D63,1px -1px 0 #3A9D63,1px 0px 0 #3A9D63,1px 1px 0 #3A9D63,1px 2px 0 #3A9D63,2px -2px 0 #3A9D63,2px -1px 0 #3A9D63,2px 0px 0 #3A9D63,2px 1px 0 #3A9D63,2px 2px 0 #3A9D63
+}
+.role-card[data-role="volunteer"] .title-container {
+  background-color: #E9FFE6;
+  border: 3px solid #3A9D63;
+}
+
 .role-description {
   text-align: left;
 }
--- frontend/src/components/SplashScreen.vue
+++ frontend/src/components/SplashScreen.vue
@@ -3,29 +3,35 @@
     <b-row>
       <b-col md="4" class="mb-4">
         <CardButton
+          data-role="hacker" 
           role-title="HACKER"
           role-description="As a hacker, you'll be at the forefront of problem-solving 
           and innovation. Dive into challenging projects, showcase your coding skills, 
           and be part of a community dedicated to pushing the boundaries of technology."
           role-u-r-l="/hacker"
+          svg-image-path="red-marshie.svg" 
         />
       </b-col>
       <b-col md="4" class="mb-4">
         <CardButton
+          data-role="mentor" 
           role-title="MENTOR"
           role-description="As a Mentor, leverage your expertise to guide and inspire 
           the next generation of tech enthusiasts. Become a key figure in shaping the 
           experience for participants as they navigate through the hackathon challenges."
           role-u-r-l="/mentor"
+          svg-image-path="blue-marshie.svg" 
         />
       </b-col>
       <b-col md="4" class="mb-4">
         <CardButton
+          data-role="volunteer" 
           role-title="VOLUNTEER"
           role-description="As a Volunteer, your support ensures the smooth execution
           of our hackathon. From logistics to participant assistance, your
           contribution will help create a positive environment for everyone!"
           role-u-r-l="/volunteer"
+          svg-image-path="green-marshie.svg" 
         />
       </b-col>
     </b-row>
@@ -35,6 +41,7 @@
 <script>
 import CardButton from "../components/CardButton.vue";
 
+
 export default {
   components: {
     CardButton,
@@ -43,6 +50,7 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@500&display=swap');
 .screen-container {
   align-items: center;
   justify-content: center;
@@ -53,17 +61,11 @@ export default {
 
 .role-card {
   display: flex;
-  cursor: pointer;
   flex-direction: column;
   margin: 20px 0;
   padding-top: 1.5rem;
   height: 100%;
   transition: transform 0.3s ease;
-}
-
-.role-card:hover {
-  transform: scale(1.05);
-  /* Increase the scale on hover */
 }
 
 @media (max-width: 768px) {
