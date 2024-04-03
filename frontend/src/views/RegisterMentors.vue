efbd3c70a8bf0112ebe6754a06143b1236a01a7f
eileendchen <eileendchen@gmail.com>
2024-04-02 21:48:54 -0400

    close volunteer registration

--- frontend/src/views/RegisterMentors.vue
+++ frontend/src/views/RegisterMentors.vue
@@ -2,20 +2,20 @@
     <div class="home">
         <b-container>
             <b-card class="main-card mt-3 mt-lg-4 mt-xxl-5">
-                <!-- <registration-form-mentor /> -->
-                <registration-closed-other />
+                <registration-form-mentor />
+                <!-- <registration-closed-other /> -->
             </b-card>
         </b-container>
     </div>
 </template>
 
 <script>
-// import RegistrationFormMentor from "../components/RegistrationFormMentor.vue";
-import RegistrationClosedOther from "../components/RegistrationClosedOther.vue"
+import RegistrationFormMentor from "../components/RegistrationFormMentor.vue";
+// import RegistrationClosedOther from "../components/RegistrationClosedOther.vue"
 export default {
     name: "RegisterView",
-    // components: { RegistrationFormMentor },
-    components: { RegistrationClosedOther },
+    components: { RegistrationFormMentor },
+    // components: { RegistrationClosedOther },
     props: {
         default_track: {
             type: String,
