5f4a33c976cf8abfa73f85a2aefbc0c8b430e0cb
Ananya Srinivasan <ananya.srinivasan@gmail.com>
2026-01-26 16:00:04 -0500

    made other dropdown signature hidden, added other student consent form

--- frontend/src/components/MinorForms.vue
+++ frontend/src/components/MinorForms.vue
@@ -29,7 +29,7 @@
 
         <div class="info-box">
             <div class="info-header" @click="toggleStu">
-                <span class="title" style="color: white">Student Organization Release and Informed Consent</span>
+                <span class="title" style="color: white">Student Organization Release and Informed Consent Form</span>
                     <img
                     :src="openImg"
                     class="arrow"
@@ -127,6 +127,7 @@
         </div>
     </div>
 
+  <div v-if="photoIsOpen">
     <b-form-row>
         <b-form-group class="col-md-6">
           <template #label> Minor Full Name <span class="text-danger">*</span></template>
@@ -212,6 +213,190 @@
           </b-form-invalid-feedback>
         </b-form-group>
     </b-form-row>
+    <br>
+    </div>
+
+    <div class="info-box">
+            <div class="info-header" @click="toggleStu2">
+                <span class="title" style="color: white">Student Organization Release and Informed Consent Form</span>
+                    <img
+                    :src="openImg"
+                    class="arrow"
+                    :class="{open : stu2IsOpen}"
+                    @click.stop="toggleStu2"
+                    />
+                </div>
+
+        <div v-if="stu2IsOpen" class="info-content">
+        <p>
+            BY PARTICIPATING IN BITCAMP, YOU AGREE TO THE FOLLOWING TERMS AND ALL OTHER APPLICABLE 
+            DOCUMENTS.
+            <br>
+            <br>
+            Henceforth, "I", “me”, "my", “myself”, and other first-person pronouns shall refer to the Participant. "The 
+            Organizers" shall refer to any persons or group who had significant participation in the creation of the event,
+            namely (but not necessarily exclusively) the Bitcamp organizing team, Bitcamp Inc., Startup Shell Inc., the
+            student organization named Terrapin Hackers, and the University of Maryland at College Park. “The
+            Hackathon” shall refer to the Bitcamp hackathon. 
+            <br>
+            <br>
+            I understand that by participating in The Hackathon, I agree to the following terms listed in this Bitcamp 
+            Terms and Code of Conduct agreement (the “Agreement”), and to follow any procedures and instructions 
+            given by The Organizers. Refusal to comply and any violations will subject me to punishment deemed 
+            appropriate by The Organizers, such as the revocation of prizes and expulsion from The Hackathon.
+            I have represented myself accurately on the application and all other event forms. I am allowed to develop a 
+            concept for a project prior to The Hackathon, but any and all programming and tangible creations related to 
+            a concept must be done at the time and place of The Hackathon. I am allowed to use publicly available 
+            resources and code on my project, and I accept sole responsibility for licensee or owner responsibilities and 
+            for any and all licensing or ownership rights and responsibilities of any intellectual property that I use, 
+            display, store, distribute, make derivative works from, or otherwise and other appropriate acknowledgement 
+            regarding the resources. I understand that The Organizers will not receive any ownership title or rights of any 
+            work or project done at The Hackathon other than those limited rights herein. All original work will solely 
+            remain under the ownership of the creator and/or the employing company. The Organizers are not liable for 
+            any legal or contractual issues that may arise from the work. I hereby agree to indemnify, hold harmless, and 
+            defend The Organizers against any liability resulting from the project or any created content. I understand 
+            that the judges and The Organizers are allowed to look at my code for the purpose of judging by volunteer 
+            judges, and that the decisions of these judges are final and may not be contested. The Hackathon will not be 
+            held responsible for any decisions made by sponsors, and The Organizers are not liable for any transaction 
+            of prizes between a sponsor and a participant. I understand that if I win a Bitcamp-sponsored prize, it is my 
+            responsibility to be present at the event to receive the prize. If I am not present, I understand that I will not 
+            receive the prize, regardless if the rest of my team is present to receive their prizes. I understand that I will 
+            be fully responsible for any costs I incur as a direct or indirect result of participation in the event, including 
+            but not limited to use of proprietary technology, food, accommodations, and other purchases. The 
+            Organizers will not be liable for any costs or expenses incurred throughout the event. I understand that The 
+            Organizers are not obligated to give travel reimbursements, and that any granted travel reimbursements are 
+            contingent upon my submission of a project to Devpost. I understand that all services provided, including 
+            but not limited to transportation, facilities (property, furniture, electricity, networking), food and beverage, 
+            and activities are provided on an as-is and as-available basis only, with no guarantee of services meeting my 
+            requirements or remaining uninterrupted, error-free, secure, or free from viruses and other harmful 
+            components. The Organizers are not liable for any errors in services, regardless of whether they appear to
+            provide these services or not. I understand any information I provide via my application may be distributed 
+            to sponsors for recruiting purposes. I understand that by providing my resume via the application, I am 
+            allowing the distribution of my resume to sponsors for recruiting purposes. I understand that unless I have
+            paid for certain perks of sponsorship of Bitcamp, I am not allowed to recruit from the participants or disperse 
+            marketing or recruiting materials to anyone at The Hackathon, including anyone in the venue. I agree to be 
+            respectful and courteous to The Organizers, sponsors, volunteers, mentors, venue staff, and all other 
+            participants. The Organizers have the authority to discipline me if my behavior at the event does not meet 
+            their subjective standards of respect and courtesy. I authorize The Organizers in advance of any reasonably 
+            perceived medical emergency to authorize any specific diagnosis, treatment, or hospital care that is required 
+            to save my life or my child's life or to avoid or respond to serious bodily harm, but this authorization is given 
+            to provide authority and power on the part of Bitcamp, its agents and employees for the duration of a 
+            hackathon event and no more than eight hours after the official end of the event while I or my child remain 
+            on the premises of the Organizer's hackathon, in order to give specific consent to any and all such diagnosis, 
+            treatment or hospital care which the aforementioned health professional in the exercise of his or her best 
+            judgment may deem required or reasonably recommended. I agree to not be in the possession of illegal 
+            drugs, alcohol, or weapons at The Hackathon and otherwise always adhere to applicable state and federal 
+            contacts law. I understand that I am bound to all rules and regulations of Maryland state laws, as well as 
+            applicable city and federal laws.
+            <br>
+            <br>
+            I ASSUME ALL RESPONSIBILITY FOR THE DAMAGE OR THEFT OF ALL PERSONAL ITEMS AND PROPERTY. 
+            I UNDERSTAND THAT THE HACKATHON AND THE ORGANIZERS DO NOT TAKE RESPONSIBILITY AND 
+            ARE NOT LIABLE FOR ANY DAMAGES OR THEFT TO PERSONAL ITEMS AND/OR PROPERTY.
+            <br>
+            <br>
+            I understand that The Hackathon is 36 hours long, and that fatigue can cause delayed reaction time or 
+            unusual decision-making, and I indemnify and hold harmless Bitcamp and the University of Maryland for any 
+            liabilities or damages from and against, and shall compensate and reimburse each Indemnitee for, any loss, 
+            damage, injury, harm, detriment, lost opportunity, liability, exposure, claim, demand, settlement, judgment, 
+            award, fine, penalty, tax, fee (including attorneys' fees), charge or expense that is directly or indirectly 
+            suffered or incurred by any of the Indemnitees, or to which any of the Indemnitees otherwise may become 
+            subject (regardless of whether or not related to a third-party claim) at any time (whether during or after the 
+            Term), and that is caused by or results from (a) any inaccuracy in any representation or warranty by or on my 
+            behalf contained in this Agreement, or (b) any failure on my part to observe, perform or abide by, or any 
+            other breach of, any restriction, covenant, obligation or other provision contained in this Agreement.
+          </p>
+        </div>
+    </div>
+
+  <div v-if="stu2IsOpen">
+    <b-form-row>
+        <b-form-group class="col-md-6">
+          <template #label> Minor Full Name <span class="text-danger">*</span></template>
+          <b-form-input
+            id="input-minor-name"
+            v-model="formData.stu_minor_name"
+            @input="touched.stu_minor_name = true"
+            :state="showState('stu_minor_name')"
+          />
+          <b-form-invalid-feedback :state="showState('stu_minor_name')">
+            Required field
+          </b-form-invalid-feedback>
+        </b-form-group>
+        <b-form-group class="col-md-6">
+          <template #label> Date <span class="text-danger">*</span></template>
+          <b-form-input
+                  id="minor-date"
+                  v-model="formData.stu_minor_date"
+                  placeholder="MM/DD/YYYY"
+                  :state="showState('stu_minor_date')"
+                  @input="touched.stu_minor_date = true"
+                />
+                <b-form-invalid-feedback :state="showState('stu_minor_date')">
+                  Please enter today's date
+                </b-form-invalid-feedback>
+        </b-form-group>
+    </b-form-row>
+
+    <b-form-row>
+        <b-form-group class="col-md-12">
+          <template #label> Signature <span class="text-danger">*</span></template>
+          <b-form-input
+            id="input-minor_sig"
+            v-model="formData.stu_minor_sig"
+            @input="touched.stu_minor_sig = true"
+            :state="showState('stu_minor_sig')"
+          />
+          <b-form-invalid-feedback :state="showState('stu_minor_sig')">
+            Required field
+          </b-form-invalid-feedback>
+        </b-form-group>
+    </b-form-row>
+
+    <b-form-row>
+        <b-form-group class="col-md-6">
+          <template #label> Parent/Guardian Full Name <span class="text-danger">*</span></template>
+          <b-form-input
+            id="input-parent_name"
+            v-model="formData.stu_parent_name"
+            @input="touched.stu_parent_name = true"
+            :state="showState('stu_parent_name')"
+          />
+          <b-form-invalid-feedback :state="showState('stu_parent_name')">
+            Required field
+          </b-form-invalid-feedback>
+        </b-form-group>
+        <b-form-group class="col-md-6">
+          <template #label> Date <span class="text-danger">*</span></template>
+            <b-form-input
+                  id="parent-date"
+                  v-model="formData.stu_parent_date"
+                  placeholder="MM/DD/YYYY"
+                  :state="showState('stu_parent_date')"
+                  @input="touched.stu_parent_date = true"
+            />
+            <b-form-invalid-feedback :state="showState('stu_parent_date')">
+                Please enter today's date
+            </b-form-invalid-feedback>
+        </b-form-group>
+    </b-form-row>
+
+    <b-form-row>
+        <b-form-group class="col-md-12">
+          <template #label> Signature <span class="text-danger">*</span></template>
+          <b-form-input
+            id="input-parent_sig"
+            v-model="formData.stu_parent_sig"
+            @input="touched.stu_parent_sig = true"
+            :state="showState('stu_parent_sig')"
+          />
+          <b-form-invalid-feedback :state="showState('stu_parent_sig')">
+            Required field
+          </b-form-invalid-feedback>
+        </b-form-group>
+    </b-form-row>
+    <br>
+    </div>
 
     <div class="info-box">
             <div class="info-header" @click="toggleCond">
@@ -399,7 +584,19 @@ export default {
         "parent_name",
         "parent_date",
         "parent_sig",
-        "chaperone"
+        "stu_minor_name",
+        "stu_minor_date",
+        "stu_minor_sig",
+        "stu_parent_name",
+        "stu_parent_date",
+        "stu_parent_sig",
+        "chaperone",
+        "chap_minor_name",
+        "chap_minor_date",
+        "chap_minor_sig",
+        "chap_name",
+        "chap_date",
+        "chap_sig"
     ];
 
     return {
@@ -410,6 +607,12 @@ export default {
         "parent_name",
         "parent_date",
         "parent_sig",
+        "stu_minor_name",
+        "stu_minor_date",
+        "stu_minor_sig",
+        "stu_parent_name",
+        "stu_parent_date",
+        "stu_parent_sig",
         "chaperone",
         "chap_minor_name",
         "chap_minor_date",
@@ -432,6 +635,7 @@ export default {
 
       stuIsOpen: false,
       photoIsOpen: false,
+      stu2IsOpen: false,
       condIsOpen: false,
       openImg: require('@/assets/dropdown-icons/up-arrow.svg')
     };
@@ -448,6 +652,12 @@ export default {
             parent_name: req(this.formData.parent_name),
             parent_date: req(this.formData.parent_date) && this.isValidDateFormat(this.formData.parent_date),
             parent_sig: req(this.formData.parent_sig),
+            stu_minor_name: req(this.formData.stu_minor_name),
+            stu_minor_date: req(this.formData.stu_minor_date) && this.isValidDateFormat(this.formData.stu_minor_date),
+            stu_minor_sig: req(this.formData.stu_minor_sig),
+            stu_parent_name: req(this.formData.stu_parent_name),
+            stu_parent_date: req(this.formData.stu_parent_date) && this.isValidDateFormat(this.formData.stu_parent_date),
+            stu_parent_sig: req(this.formData.stu_parent_sig),
             chaperone: req(this.formData.chaperone),
             chap_minor_name: req(this.formData.chap_minor_name),
             chap_minor_date: req(this.formData.chap_minor_date) && this.isValidDateFormat(this.formData.chap_minor_date),
@@ -539,6 +749,10 @@ export default {
 
     togglePhoto() {
       this.photoIsOpen = !this.photoIsOpen;
+    },
+
+    toggleStu2() {
+      this.stu2IsOpen = !this.stu2IsOpen;
     },
     
     toggleCond(){
